bc40a0ea7e7326a6741e6506a09a252155c7fbe6
Bar.Wachtel <bar.wachtel@myob.com>
2021-03-02 18:34:22 +1100

    Add fetch Landmarks from backend

--- src/Page.js
+++ src/Page.js
@@ -1,23 +1,29 @@
-import Destinations from "./Destinations";
+import Destinations from "./blog/Destinations";
+import {useState, useEffect} from "react";
+import axios from 'axios';
 
 const LandingPage = () => {
+    const [destinations, setDestinations] = useState(null);
+
+    const apiURL = "https://bar-backend-interview.herokuapp.com/";
+
+    const fetchData = async () => {
+        const response = await axios.get(apiURL)
+        console.log(response);
+
+        setDestinations(response.data.landmarks)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [destinations])
+
     return <div>
         <h1>Welcome to Bar's Bouldering Blog!</h1>
         <p>Checkout some of these great bouldering destinations</p>
-        <Destinations destinations={[
-            {
-                title: 'Grampians',
-                imgUrl: 'https://www.theage.com.au/long-reads/our-patch/grampians/img/HR-pinnacle_thomasparkes.jpg',
-                description: 'Short drive from Melbourne, with amazing sights!',
-                id: 1
-            },
-            {
-                title: 'Nowra',
-                imgUrl: 'https://moredaysoff.files.wordpress.com/2017/01/james-on-cover-boy_e.jpg',
-                description: 'A bit out of the way but a great climbing spot',
-                id: 2
-            },
-        ]}/>
+        {destinations == null || <h3>Loading...</h3>}
+
+        {destinations != null && <Destinations destinations={destinations}/>}
     </div>
 }
 
--- src/blog/Destination.js
+++ src/blog/Destination.js
@@ -1,9 +1,9 @@
 const Destination = ({destination}) => {
-    const {title, imgUrl, description, id} = destination;
+    const {title, image_url, description, id} = destination;
     return <ul key={id}>
         <div>
             <h2>{title}</h2>
-            <img src={imgUrl} width={500} height={400}/>
+            <img src={image_url} width={500} height={400}/>
             <p>{description}</p>
         </div>
     </ul>
